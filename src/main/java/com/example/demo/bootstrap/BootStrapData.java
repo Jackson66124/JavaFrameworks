06dbf81808ffc1d9b1714ad334a6fa9c983443e0
Jack <jkigh23@wgu.edu>
2024-02-13 17:53:39 -0500

    Updated BootStrapData with if statement.

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -41,25 +41,43 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        InhousePart joystick= new InhousePart();
-        joystick.setName("Joystick"); joystick.setInv(23); joystick.setPrice(5.00); joystick.setId(100L);
-        partRepository.save(joystick);
+        if(partRepository.count() == 0) {
+            InhousePart joystick = new InhousePart();
+            joystick.setName("Joystick");
+            joystick.setInv(23);
+            joystick.setPrice(5.00);
+            joystick.setId(100L);
+            partRepository.save(joystick);
 
-        InhousePart trigger= new InhousePart();
-        trigger.setName("Trigger"); trigger.setInv(14); trigger.setPrice(8.00); trigger.setId(101L);
-        partRepository.save(trigger);
+            InhousePart trigger = new InhousePart();
+            trigger.setName("Trigger");
+            trigger.setInv(14);
+            trigger.setPrice(8.00);
+            trigger.setId(101L);
+            partRepository.save(trigger);
 
-        InhousePart button= new InhousePart();
-        button.setName("Button"); button.setInv(25); button.setPrice(3.00); button.setId(102L);
-        partRepository.save(button);
+            InhousePart button = new InhousePart();
+            button.setName("Button");
+            button.setInv(25);
+            button.setPrice(3.00);
+            button.setId(102L);
+            partRepository.save(button);
 
-        InhousePart dpad= new InhousePart();
-        dpad.setName("D-Pad"); dpad.setInv(20); dpad.setPrice(10.00); dpad.setId(103L);
-        partRepository.save(dpad);
+            InhousePart dpad = new InhousePart();
+            dpad.setName("D-Pad");
+            dpad.setInv(20);
+            dpad.setPrice(10.00);
+            dpad.setId(103L);
+            partRepository.save(dpad);
 
-        InhousePart bumper= new InhousePart();
-        bumper.setName("Bumper"); bumper.setInv(12); bumper.setPrice(7.00); bumper.setId(104L);
-        partRepository.save(bumper);
+            InhousePart bumper = new InhousePart();
+            bumper.setName("Bumper");
+            bumper.setInv(12);
+            bumper.setPrice(7.00);
+            bumper.setId(104L);
+            partRepository.save(bumper);
+        }
+
 
         OutsourcedPart thePart=null;
         List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
@@ -73,19 +91,19 @@ public class BootStrapData implements CommandLineRunner {
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
 
+        if(productRepository.count() == 0) {
+            Product xboxController = new Product("Xbox Controller", 60.0, 20);
+            Product playStationController = new Product("Playstation Controller", 60.0, 20);
+            Product switchController = new Product("Switch Controller", 50.0, 25);
+            Product playStationProController = new Product("Playstation Pro Controller", 90.0, 15);
+            Product xboxProController = new Product("Xbox Pro Controller", 90.0, 15);
 
-        Product xboxController= new Product("Xbox Controller",60.0,20);
-        Product playStationController= new Product("Playstation Controller",60.0,20);
-        Product switchController= new Product("Switch Controller",50.0,25);
-        Product playStationProController= new Product("Playstation Pro Controller",90.0,15);
-        Product xboxProController= new Product("Xbox Pro Controller",90.0,15);
-
-        productRepository.save(xboxController);
-        productRepository.save(playStationController);
-        productRepository.save(switchController);
-        productRepository.save(playStationProController);
-        productRepository.save(xboxProController);
-
+            productRepository.save(xboxController);
+            productRepository.save(playStationController);
+            productRepository.save(switchController);
+            productRepository.save(playStationProController);
+            productRepository.save(xboxProController);
+        }
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Products"+productRepository.count());
